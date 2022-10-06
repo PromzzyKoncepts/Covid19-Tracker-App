844cda7ddf0148fb7c1978b7ac5949dabb2cbf8d
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-10-06 15:41:21 +0100

    Update file for implementing slides

--- src/components/search.js
+++ src/components/search.js
@@ -3,10 +3,18 @@ import { Form } from "react-bootstrap";
 import { InputGroup } from "react-bootstrap";
 import "./slide.css";
 
-const Search = () => {
+const Search = ({ data, setCountries }) => {
+
+  const handleSearch = (e) => {
+    console.log('we are here', e.target.value);
+    const searchResults = data.filter((item) =>
+      item.countryRegion.toLowerCase().includes(e.target.value.toLowerCase()));
+    console.log(searchResults, 'the results of the search');
+    setCountries(searchResults);
+  };
   return (
     <div className="search">
-      <InputGroup size="lg" className="center">
+      <InputGroup size="lg" onChange={handleSearch} className="center" >
         <InputGroup.Text id="inputGroup-sizing-lg">Search</InputGroup.Text>
         <Form.Control
           aria-label="Large"

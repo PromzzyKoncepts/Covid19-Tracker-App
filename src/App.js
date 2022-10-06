b3f0f378e87391f2a32b171ddafddfe664970e0f
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-10-06 15:40:39 +0100

    Update file for implementing popups

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Popup from "./components/Popup";
 
 function App() {
   return (
     <Routes>
       <Route path="/" exact element={<Home />} />
+      <Route path="/country/:id" exact element={<Popup />} />
     </Routes>
   );
 }

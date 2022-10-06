c03889b8142e2c56d60ee0e76db929c9b0e677aa
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-10-06 15:43:39 +0100

    Update file for implementing popups

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,17 +1,35 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchData } from "../redux/apicontainer";
+import PopupDetails from "./PopupDetails";
+
 
 const Popup = () => {
   const { data } = useSelector((state) => state.log);
+  const { id } = useParams();
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchData());
   }, [dispatch]);
   return (
-    <div>
-      <h3>Covid19 Data</h3>
+    <div className="popup-container">
+      {data
+        .filter((item) => item.uid == id)
+        .map((item) => (
+          <PopupDetails
+            key={item.id}
+            id={item.id}
+            confirmed={item.confirmed}
+            deaths={item.deaths}
+            combinedKey={item.combinedKey}
+            countryRegion={item.countryRegion}
+            cases28Days={item.cases28Days}
+            deaths28Days={item.deaths28Days}
+          />
+        ))}
     </div>
-  )
+  );
 }
 export default Popup;

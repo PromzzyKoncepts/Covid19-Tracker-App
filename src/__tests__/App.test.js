69b2f4fdeed74bbafa18f65b75177841fa90ba08
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-10-07 11:40:28 +0100

    fix linters

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,18 +1,17 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { MemoryRouter as Router } from "react-router-dom";
-import App from "../App";
-import { Provider } from "react-redux";
-import store from "../redux/Store";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MemoryRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from '../App';
+import store from '../redux/Store';
 
-
-it("renders Component correctly", () => {
+it('renders Component correctly', () => {
   const tree = renderer.create(
     <Provider store={store}>
       <Router>
         <App />
       </Router>
-    </Provider>
+    </Provider>,
   );
   expect(tree).toMatchSnapshot();
 });
